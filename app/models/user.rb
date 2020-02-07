513cfb2e24b05a383354821b4a711762d61a653d
abasukar <59372770+abasukar@users.noreply.github.com>
2020-02-07 15:28:35 +0200

    editing bootstrap css

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :confirmable, 
        :recoverable, :rememberable, :validatable
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
   enum roles: [:user, :admin]
