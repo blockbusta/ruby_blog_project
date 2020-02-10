5fb13dad6f47ab47a776e643f6fa21e92e2dbe31
abasukar <59372770+abasukar@users.noreply.github.com>
2020-02-10 16:55:10 +0200

    minor changes in css and layout

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,5 @@
 class Car < ApplicationRecord
     validates :title, :presence => true
-    has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/not_av_m.png"
+    has_attached_file :avatar, styles: { medium: "400x400>", thumb: "100x100>" }, default_url: "/images/not_av_m.png"
     validates_attachment_content_type :avatar, content_type: ["image/jpeg", "image/gif", "image/png"]
 end
